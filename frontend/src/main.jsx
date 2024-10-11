1c39a7484170bbf9852a96fbeed31fade54d3f22
Johander <connect@johandercampos.com>
2024-10-11 02:32:58 -0300

    feat: update frontend components and styles

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from "react-router";
+import { BrowserRouter } from 'react-router';
 import '/global.css';
 import App from './App';
 
@@ -8,6 +8,7 @@ import App from './App';
 // Implementar validaciones de URL para el input que recibe links React Toastify // PENDIENTE
 // Mejorar la implementación de las rutas según buenas prácticas para react router. Tanto rutas pública como privadas // PENDIENTE
 // Revisar que los props funcionen correctamente // PENDIENTE
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
