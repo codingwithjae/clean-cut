2e693aaf3c1ee7ff1d61052a83d2344653b83db0
Johander <connect@johandercampos.com>
2025-01-08 11:38:22 -0300

    feat: finalize core application features

--- backend/index.js
+++ backend/index.js
@@ -6,22 +6,16 @@ const { requestLogger } = require('./middlewares/request.logger.js');
 const { notFound, errorHandler } = require('./middlewares/errors.handler.js');
 const cacheMiddleware = require('./middlewares/cache.handler.js');
 const cors = require('cors');
-// const morgan = require('morgan');
 
 const app = express();
 const port = 4000;
 
-// PENDIENTE: MEJORAR EL ESQUEMA DE DATOS CON HAPI
-// PENDIENTE: CONECTAR EL FRONTEND
-
-// app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
 app.use(rateLimiter);
 app.use(requestLogger);
 
-// Configurar el middleware de caché para todas las rutas
-// El parámetro 300 significa que la caché expirará después de 300 segundos (5 minutos)
+
 app.use(cacheMiddleware(300));
 
 routerApi(app);
